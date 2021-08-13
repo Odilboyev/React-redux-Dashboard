b121f59bb834f29b2c4eacde886ce2464db2b023
Odilboyev <samandarodilboyev@gmail.com>
2021-08-13 19:28:04 +0500

    fixed browserlist problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.css'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
